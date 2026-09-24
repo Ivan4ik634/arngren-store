e72011a3d51edd1e2a80abf4f8a56fed6c685329
White <ivanlevadny2010@gmail.com>
2026-09-24 14:08:17 +0300

    fixed in webhook from profiles

--- app/api/checkout/webhook/route.ts
+++ app/api/checkout/webhook/route.ts
@@ -49,7 +49,7 @@ export async function POST(req: NextRequest) {
       transaction: randomDeposit,
     });
     await supabase
-      .from('profile')
+      .from('profiles')
       .update({ balance: data.balance + (session.amount_total ?? 0) / 100 })
       .eq('id', session.metadata?.user_id);
 
